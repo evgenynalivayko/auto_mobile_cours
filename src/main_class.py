8e2e8c283ea4b93597bb50a7096c541d2e523aa6
Evgeny Nalivayko <nalivayko.ev@gmail.com>
2020-11-01 00:04:04 +0300

    test 3

--- src/main_class.py
+++ src/main_class.py
@@ -4,9 +4,13 @@
 
 class MainClass:
     __class_number = 20
+    __class_string = "Hello, world"
 
     def get_local_number(self):
         return 14
 
     def get_class_number(self):
         return self.__class_number
+
+    def get_class_string(self):
+        return self.__class_string
--- src/test_main_class.py
+++ src/test_main_class.py
@@ -11,3 +11,12 @@ class TestMainClass:
     def test_get_class_number(self):
         get_class_number_result = MainClass().get_class_number()
         assert get_class_number_result > 45, f'get_class_number_result вернул {get_class_number_result}, что меньше 45'
+
+    def test_get_class_string(self):
+        get_class_string_result = MainClass().get_class_string()
+        n = 0
+        sub_word = ["hello", "Hello"]
+        for word in sub_word:
+            if word in get_class_string_result:
+                n = n + 1
+        assert n > 0, f'Строка "{get_class_string_result}" не содержит подстрок {sub_word}'
