0726fb69aa457c403964383582ad1758b8041eb0
Evgeny Nalivayko <nalivayko.ev@gmail.com>
2020-10-31 23:23:39 +0300

    test 2

--- src/main_class.py
+++ src/main_class.py
@@ -3,5 +3,10 @@
 
 
 class MainClass:
+    __class_number = 20
+
     def get_local_number(self):
         return 14
+
+    def get_class_number(self):
+        return self.__class_number
--- src/test_main_class.py
+++ src/test_main_class.py
@@ -7,3 +7,7 @@ class TestMainClass:
     def test_get_local_number(self):
         get_local_number_result = MainClass().get_local_number()
         assert get_local_number_result == 14, f'метод get_local_number вернул {get_local_number_result} вместо 14'
+
+    def test_get_class_number(self):
+        get_class_number_result = MainClass().get_class_number()
+        assert get_class_number_result > 45, f'get_class_number_result вернул {get_class_number_result}, что меньше 45'
