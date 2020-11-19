ace8df4bf622dee28b25b9866e71daf4cea91f89
Evgeny Nalivayko <nalivayko.ev@gmail.com>
2020-11-19 23:25:29 +0300

    work on bugs

--- src/main_class.py
+++ src/main_class.py
@@ -3,11 +3,13 @@
 
 
 class MainClass:
+    z = 14
+    x = 45
     __class_number = 20
     __class_string = "Hello, world"
 
     def get_local_number(self):
-        return 14
+        return self.z
 
     def get_class_number(self):
         return self.__class_number
--- src/test_main_class.py
+++ src/test_main_class.py
@@ -6,17 +6,18 @@ from src.main_class import MainClass
 class TestMainClass:
     def test_get_local_number(self):
         get_local_number_result = MainClass().get_local_number()
-        assert get_local_number_result == 14, f'метод get_local_number вернул {get_local_number_result} вместо 14'
+        assert get_local_number_result == MainClass.z, f'метод get_local_number вернул {get_local_number_result} вместо {MainClass.z}'
 
     def test_get_class_number(self):
         get_class_number_result = MainClass().get_class_number()
-        assert get_class_number_result > 45, f'get_class_number_result вернул {get_class_number_result}, что меньше 45'
+        assert get_class_number_result > MainClass.x, f'get_class_number_result вернул {get_class_number_result}, что меньше {MainClass.x}'
 
     def test_get_class_string(self):
         get_class_string_result = MainClass().get_class_string()
-        n = 0
+        has_substring = False
         sub_word = ["hello", "Hello"]
         for word in sub_word:
             if word in get_class_string_result:
-                n = n + 1
-        assert n > 0, f'Строка "{get_class_string_result}" не содержит подстрок {sub_word}'
+                has_substring = True
+                break
+        assert has_substring, f'Строка "{get_class_string_result}" не содержит подстрок {sub_word}'
