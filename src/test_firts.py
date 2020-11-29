df9ea59d2f4290c52ebc921926c65cf1845db179
Evgeny Nalivayko <nalivayko.ev@gmail.com>
2020-11-30 00:03:45 +0300

    ex3 - clear search result

--- src/test_firts.py
+++ src/test_firts.py
@@ -12,6 +12,11 @@ def wait_for_element_present(driver, locator, error_message, timeout_in_second=5
     return element.until(expected_conditions.presence_of_element_located(locator), message=error_message)
 
 
+def wait_for_elements_present(driver, locator, error_message, timeout_in_second=5):
+    element = WebDriverWait(driver=driver, timeout=timeout_in_second)
+    return element.until(expected_conditions.presence_of_all_elements_located(locator), message=error_message)
+
+
 def wait_for_element_and_click(driver, locator, error_message, timeout_in_second=5):
     element = wait_for_element_present(driver=driver, locator=locator, error_message=error_message, timeout_in_second=timeout_in_second)
     element.click()
@@ -128,3 +133,36 @@ def test_text_in_search_input(appium_driver):
         error_message="Cannot find 'Search Wikipedia' input"
     )
     assert_element_has_text(element_locator, "Search Wikipedia", "Invalid text in search input!")
+
+
+def test_list_search_results_and_clear_results(appium_driver):
+    appium_driver.find_element_by_id(
+        "org.wikipedia:id/fragment_onboarding_skip_button").click()
+    wait_for_element_and_click(
+        driver=appium_driver,
+        locator=(By.XPATH, "//*[contains(@text,'Search Wikipedia')]"),
+        error_message="Cannot find 'Search Wikipedia' input"
+    )
+    wait_for_element_and_send_keys(
+        value="QA",
+        driver=appium_driver,
+        locator=(
+        By.XPATH, "//*[@resource-id='org.wikipedia:id/search_src_text']"),
+        error_message="Cannot find 'Search Wikipedia' input"
+    )
+    list_search_results = wait_for_elements_present(
+        driver=appium_driver,
+        locator=(By.XPATH, "//*[@resource-id='org.wikipedia:id/search_results_list']/child::*"),
+        error_message="Cannot find list elements result"
+    )
+    assert len(list_search_results) > 1, "Fewer than two results found"
+    wait_for_element_and_clear(
+        driver=appium_driver,
+        locator=(By.ID, "org.wikipedia:id/search_src_text"),
+        error_message="Cannot find 'Search Wikipedia' input with text or never clear"
+    )
+    wait_for_element_not_present(
+        driver=appium_driver,
+        locator=(By.XPATH, "//*[@resource-id='org.wikipedia:id/search_results_list']"),
+        error_message="Search result is still present on the page"
+    )
